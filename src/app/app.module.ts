76e015521dfd14d9870855ed48b64ef8ddfec9fe
Jeffrey Hamlin <sourcesage@gmail.com>
2018-10-17 16:04:00 -0400

    Material Design Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {WeatherService} from './weather/weather.service';
-import { HttpClientModule } from '@angular/common/http'
-
-
+import { HttpClientModule } from '@angular/common/http';
+import 'hammerjs'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CurrentWeatherComponent } from './current-weather/current-weather.component';
+
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { CurrentWeatherComponent } from './current-weather/current-weather.compo
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [
     WeatherService
